7014a054de7ccf6cda830b69a92a002e9b885d3d
Eduard Wayland <eduproard@gmail.com>
2021-07-15 20:40:14 +0200

    Removed useless comments

--- UltimateAnvil/UltimateAnvilPlugin/src/mc/ultimatecore/anvil/nms/VersionMatcher.java
+++ UltimateAnvil/UltimateAnvilPlugin/src/mc/ultimatecore/anvil/nms/VersionMatcher.java
@@ -12,38 +12,6 @@ import java.util.function.Supplier;
  * @since 1.2.1
  */
 public enum VersionMatcher {
-/*
-    private final String serverVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
-
-    private final List<VersionWrapper> versions = Arrays.asList(
-            V1_8_R3.class,
-            V1_9_R1.class,
-            V1_9_R2.class,
-            V1_10_R1.class,
-            V1_11_R1.class,
-            V1_12_R1.class,
-            V1_13_R1.class,
-            V1_13_R2.class,
-            V1_14_R1.class,
-            V1_15_R1.class,
-            V1_16_R1.class,
-            V1_16_R2.class,
-            V1_16_R3.class,
-            V1_17_R1.class
-    );
-
-
-    public VersionWrapper match() {
-        try {
-            return versions.stream()
-                    .filter(version -> version.getSimpleName().substring(7).equals(serverVersion))
-                    .findFirst().orElseThrow(() -> new RuntimeException("Your server version isn't supported in AnvilGUI!"))
-                    .newInstance();
-        } catch (IllegalAccessException | InstantiationException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-*/
     
     /**
      * IntelliJ will recommend you to replace these with method reference.
