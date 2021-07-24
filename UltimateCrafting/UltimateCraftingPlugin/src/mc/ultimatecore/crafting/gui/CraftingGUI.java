83c4707770e5546c61cbd0fc3251cbb0abaa2b22
Eduard Wayland <eduproard@gmail.com>
2021-07-24 11:11:49 +0200

    Fixed crafting dupe glitch

--- UltimateCrafting/UltimateCraftingPlugin/src/mc/ultimatecore/crafting/gui/CraftingGUI.java
+++ UltimateCrafting/UltimateCraftingPlugin/src/mc/ultimatecore/crafting/gui/CraftingGUI.java
@@ -25,12 +25,12 @@ import org.bukkit.metadata.FixedMetadataValue;
 import java.util.Optional;
 
 public class CraftingGUI extends GUI implements Listener {
-
+    
     @Getter
     private final CraftingGUIManager guiManager;
-
+    
     private final HyperCrafting plugin;
-
+    
     public CraftingGUI(Player player, HyperCrafting plugin) {
         super(plugin.getInventories().mainMenuSize, plugin.getInventories().mainMenuTitle, plugin.getInventories().decorationSlots);
         this.plugin = plugin;
@@ -39,41 +39,41 @@ public class CraftingGUI extends GUI implements Listener {
         guiManager.init(player);
         plugin.registerListeners(this);
     }
-
+    
     @Override
     public void addContent() {
         super.addContent();
         if (getInventory().getViewers().isEmpty()) return;
         setItem(plugin.getInventories().closeButton.slot, InventoryUtils.makeItem(plugin.getInventories().closeButton));
     }
-
+    
     @EventHandler
-    public void onInventoryClose(InventoryCloseEvent e){
+    public void onInventoryClose(InventoryCloseEvent e) {
         Inventory inventory = e.getInventory();
         if (!inventory.equals(getInventory())) return;
         Player player = (Player) e.getPlayer();
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            for(Integer slot : plugin.getInventories().craftingSlots){
+            for (Integer slot : plugin.getInventories().craftingSlots) {
                 ItemStack itemStack = inventory.getItem(slot);
-                if(itemStack != null && !itemStack.getType().equals(Material.AIR)){
+                if (itemStack != null && !itemStack.getType().equals(Material.AIR)) {
                     player.getInventory().addItem(itemStack);
                     getInventory().setItem(slot, null);
                 }
             }
             guiManager.init(player);
         }, 2L);
-
-
+        
+        
     }
-
+    
     @EventHandler
-    public void onInventoryDrag(InventoryDragEvent e){
+    public void onInventoryDrag(InventoryDragEvent e) {
         Inventory inventory = e.getInventory();
-        if(!inventory.equals(getInventory())) return;
+        if (!inventory.equals(getInventory())) return;
         Player player = (Player) e.getWhoClicked();
         guiManager.init(player);
     }
-
+    
     @EventHandler
     public void onInventoryDrag(InventoryOpenEvent e) {
         Inventory inventory = e.getInventory();
@@ -83,19 +83,18 @@ public class CraftingGUI extends GUI implements Listener {
             guiManager.setInventoryView(plugin.getNms().getInventoryView(player, "Special Table", player.getWorld(), player.getLocation(), new FixedMetadataValue(plugin, "UUID")));
         guiManager.init(player);
     }
-
-
-
+    
+    
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
-        Player player = (Player)e.getWhoClicked();
+        Player player = (Player) e.getWhoClicked();
         int slot = e.getSlot();
         if (e.getClickedInventory() == null || !e.getClickedInventory().equals(getInventory()) && !e.getInventory().equals(getInventory())) return;
-        if(e.getInventory().equals(getInventory()) && !e.getClickedInventory().equals(getInventory())){
+        if (e.getInventory().equals(getInventory()) && !e.getClickedInventory().equals(getInventory())) {
             guiManager.init(player);
             return;
         }
-        if(slot == plugin.getInventories().closeButton.slot) {
+        if (slot == plugin.getInventories().closeButton.slot) {
             e.setCancelled(true);
             player.closeInventory();
             return;
@@ -108,7 +107,7 @@ public class CraftingGUI extends GUI implements Listener {
                     && player.hasPermission("hypercrafting.autorecipes") && e.getClick().isLeftClick()) {
                 ItemStack cursor = e.getCursor();
                 Optional<CraftingRecipe> craftingRecipe = plugin.getCraftingRecipes().getRecipeByName(guiManager.getRecipeSlots().get(slot));
-                if (craftingRecipe.isPresent()){
+                if (craftingRecipe.isPresent()) {
                     ItemStack clicked = craftingRecipe.get().getResult().clone();
                     if (cursor != null && cursor.getType() != Material.AIR && !cursor.isSimilar(clicked) || e.isShiftClick() || e.isRightClick())
                         return;
@@ -121,7 +120,7 @@ public class CraftingGUI extends GUI implements Listener {
                             finalItem.setAmount(cursor.getAmount() + finalItem.getAmount());
                             Bukkit.getScheduler().runTaskLater(plugin, () -> player.setItemOnCursor(finalItem), 1);
                         }
-                    }else if (autoReturn == AutoReturn.NO_ITEMS) {
+                    } else if (autoReturn == AutoReturn.NO_ITEMS) {
                         player.sendMessage(Utils.color(plugin.getMessages().getMessage("noItems")));
                     }
                 }
@@ -130,38 +129,38 @@ public class CraftingGUI extends GUI implements Listener {
         }
         ItemStack itemStack = guiManager.getDisplayItem();
         ItemStack cursor = e.getCursor();
-        if(slot == 23){
+        if (slot == 23) {
             e.setCancelled(true);
-            if(!checkItems(itemStack, cursor)) return;
-            if(!Utils.itemIsNull(itemStack) && itemStack.getType() == Material.BARRIER) return;
-            if(e.isShiftClick()){
-                if(!reachStackSize(cursor, itemStack)){
+            if (!checkItems(itemStack, cursor)) return;
+            if (!Utils.itemIsNull(itemStack) && itemStack.getType() == Material.BARRIER) return;
+            if (e.isShiftClick()) {
+                if (!reachStackSize(cursor, itemStack)) {
                     Bukkit.getServer().getPluginManager().callEvent(new HyperCraftEvent(player, e.getCurrentItem()));
                     ItemStack newItem = itemStack.clone();
                     int maxQuantity = guiManager.getMinItemQuantity(newItem);
-                    for(int i = 0; i<maxQuantity; i++)
+                    for (int i = 0; i < maxQuantity; i++)
                         Bukkit.getScheduler().runTaskLater(plugin, () -> guiManager.removeItem(player, itemStack, true), 1L);
-                    if(maxQuantity > newItem.getMaxStackSize()){
-                        for(int i = 0; i<maxQuantity; i++)
+                    if (maxQuantity > newItem.getMaxStackSize()) {
+                        for (int i = 0; i < maxQuantity; i++)
                             player.getInventory().addItem(newItem);
-                    }else{
+                    } else {
                         newItem.setAmount(newItem.getAmount() * maxQuantity);
                         player.getInventory().addItem(newItem);
                     }
                 }
-            }else{
+            } else {
                 Bukkit.getScheduler().runTaskLater(plugin, () -> guiManager.removeItem(player, itemStack, false), 1L);
             }
         }
         guiManager.init(player);
     }
-
-    private boolean checkItems(ItemStack itemStack, ItemStack cursor){
+    
+    private boolean checkItems(ItemStack itemStack, ItemStack cursor) {
         return Utils.itemIsNull(cursor) || !Utils.itemIsNull(cursor) && cursor.isSimilar(itemStack);
     }
-
-    private boolean reachStackSize(ItemStack cursor, ItemStack secondItem){
-        if(cursor == null || cursor.getType().equals(Material.AIR))
+    
+    private boolean reachStackSize(ItemStack cursor, ItemStack secondItem) {
+        if (cursor == null || cursor.getType().equals(Material.AIR))
             return false;
         return secondItem != null && cursor.getAmount() + secondItem.getAmount() >= cursor.getMaxStackSize();
     }
