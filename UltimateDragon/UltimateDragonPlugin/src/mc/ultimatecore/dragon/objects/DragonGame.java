3767432e0dea7c7ade3d880a1e0e3728457658b6
Eduard Wayland <eduproard@gmail.com>
2021-07-25 14:04:35 +0200

    Fixing asynchronous entity add

--- UltimateDragon/UltimateDragonPlugin/src/mc/ultimatecore/dragon/objects/DragonGame.java
+++ UltimateDragon/UltimateDragonPlugin/src/mc/ultimatecore/dragon/objects/DragonGame.java
@@ -14,6 +14,7 @@ import mc.ultimatecore.dragon.objects.others.TimerPart;
 import mc.ultimatecore.dragon.objects.structures.DragonAltar;
 import mc.ultimatecore.dragon.objects.structures.DragonStructure;
 import mc.ultimatecore.dragon.utils.StringUtils;
+import mc.ultimatecore.dragon.utils.TaskUtilities;
 import mc.ultimatecore.dragon.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -33,6 +34,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Getter
 @Setter
 public class DragonGame {
+    
     private IHyperDragon hyperDragon;
     private UCEnderDragon enderDragon;
     private Location spawn;
@@ -40,21 +42,21 @@ public class DragonGame {
     private final List<Entity> crystals = new ArrayList<>();
     private boolean active = false;
     private final HyperDragons plugin = HyperDragons.getInstance();
-
+    
     private Integer moveDragonTask;
     private Integer eventTask;
-
+    
     private SwitchableEvent dragonEvent;
-
+    
     private ArmorStand stand;
     private BossBarObj bossBar;
     private UUID last;
-
-    public void init(){
+    
+    public void init() {
         DragonStructure dragonStructure = plugin.getDragonManager().getDragonStructure();
-        if(spawn == null) spawn = dragonStructure.getSpawnLocation();
+        if (spawn == null) spawn = dragonStructure.getSpawnLocation();
         hyperDragon = plugin.getConfiguration().randomDragon.getDragon();
-        if(hyperDragon == null){
+        if (hyperDragon == null) {
             Bukkit.getConsoleSender().sendMessage(StringUtils.color("&e[UltimateCore-Dragon] &cInvalid dragon, check dragon to spawn!"));
             return;
         }
@@ -62,9 +64,9 @@ public class DragonGame {
         this.active = true;
         this.bossBar = new BossBarObj(this);
     }
-
-    public void finish(){
-        if(enderDragon != null) {
+    
+    public void finish() {
+        if (enderDragon != null) {
             enderDragon.setPhase(EnderDragon.Phase.HOVER);
             enderDragon.setPhase(EnderDragon.Phase.DYING);
         }
@@ -74,32 +76,32 @@ public class DragonGame {
         new FinalPart(plugin, eventPlayers, last).sendFinishMessage(hyperDragon);
         active = false;
     }
-
-    public void managePlayerDamage(Player player, Double damage){
+    
+    public void managePlayerDamage(Player player, Double damage) {
         Optional<EventPlayer> eventPlayer = eventPlayers.stream()
-                .filter(evPlayer -> evPlayer.getUuid().equals(player.getUniqueId()))
-                .findFirst();
-        if(eventPlayer.isPresent())
+                                                        .filter(evPlayer -> evPlayer.getUuid().equals(player.getUniqueId()))
+                                                        .findFirst();
+        if (eventPlayer.isPresent())
             eventPlayer.get().setDamage(eventPlayer.get().getDamage() + damage);
         else
             eventPlayers.add(new EventPlayer(player.getUniqueId(), damage));
     }
-
-    public void removePlayersDamage(Double damage){
+    
+    public void removePlayersDamage(Double damage) {
         eventPlayers.forEach(eventPlayer -> eventPlayer.setDamage(eventPlayer.getDamage() - damage));
     }
-
-    public void sendMessage(String msg){
-        if(msg.equals("")) return;
+    
+    public void sendMessage(String msg) {
+        if (msg.equals("")) return;
         eventPlayers.forEach(player -> player.getPlayer().sendMessage(StringUtils.color(msg.replace("%prefix%", plugin.getConfiguration().prefix))));
     }
-
-    private void switchEvents(){
+    
+    private void switchEvents() {
         dragonEvent = new SwitchableEvent(plugin.getDragonEvents().getEvents(hyperDragon.getId()));
         eventTask = Bukkit.getServer().getScheduler().scheduleAsyncRepeatingTask(plugin, () -> {
             if (dragonEvent.getCurrentEvent() == null || !dragonEvent.getCurrentEvent().isActive()) {
                 IDragonEvent nextEvent = dragonEvent.getNext();
-                if(nextEvent != null) {
+                if (nextEvent != null) {
                     nextEvent.setEnderDragon(enderDragon);
                     nextEvent.start();
                     sendMessage(Utils.getStartMessage(nextEvent));
@@ -107,20 +109,19 @@ public class DragonGame {
             }
         }, 0, 20);
     }
-
-
-    private void moveDragon(){
+    
+    
+    private void moveDragon() {
         int maxDistance = plugin.getConfiguration().maxDistance;
         BukkitScheduler scheduler = Bukkit.getScheduler();
         moveDragonTask = scheduler.scheduleAsyncRepeatingTask(plugin, () -> {
-            if(!enderDragon.stillAlive()) return;
+            if (!enderDragon.stillAlive()) return;
             double distance = enderDragon.getLocation().distance(spawn);
-            if(distance > maxDistance) enderDragon.teleport(spawn);
+            if (distance > maxDistance) enderDragon.teleport(spawn);
         }, 0, 20);
     }
-
-
-
+    
+    
     private void makeBlockExplosion() {
         DragonStructure dragonStructure = plugin.getDragonManager().getDragonStructure();
         World w = spawn.getWorld();
@@ -144,54 +145,56 @@ public class DragonGame {
                 }
             });
         }).thenRun(() -> Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> {
-                falling.forEach(this::checkFallingBlock);
-                falling.clear();
-                dragonStructure.getAltars().forEach(DragonAltar::clearEye);
-            }, 60)).thenRun(this::initTasks);
+            falling.forEach(this::checkFallingBlock);
+            falling.clear();
+            dragonStructure.getAltars().forEach(DragonAltar::clearEye);
+        }, 60)).thenRun(this::initTasks);
     }
-
-    private void initTasks(){
+    
+    private void initTasks() {
         moveDragon();
         switchEvents();
     }
-
-    private void spawnStand(){
-        stand = spawn.getWorld().spawn(spawn, ArmorStand.class);
-        stand.setSmall(true);
-        stand.setGravity(false);
-        stand.setVisible(false);
-        stand.setInvulnerable(true);
+    
+    private void spawnStand() {
+        TaskUtilities.runSync(() -> {
+            stand = spawn.getWorld().spawn(spawn, ArmorStand.class);
+            stand.setSmall(true);
+            stand.setGravity(false);
+            stand.setVisible(false);
+            stand.setInvulnerable(true);
+        });
     }
-
-    private void cancelTask(){
+    
+    private void cancelTask() {
         BukkitScheduler scheduler = Bukkit.getScheduler();
-        if(moveDragonTask != null) scheduler.cancelTask(moveDragonTask);
-        if(eventTask != null) scheduler.cancelTask(eventTask);
-        if(dragonEvent.getCurrentEvent() != null) dragonEvent.getCurrentEvent().end();
-        if(bossBar != null) bossBar.stop();
+        if (moveDragonTask != null) scheduler.cancelTask(moveDragonTask);
+        if (eventTask != null) scheduler.cancelTask(eventTask);
+        if (dragonEvent.getCurrentEvent() != null) dragonEvent.getCurrentEvent().end();
+        if (bossBar != null) bossBar.stop();
     }
-
-    private void checkFallingBlock(FallingBlock fallingBlock){
+    
+    private void checkFallingBlock(FallingBlock fallingBlock) {
         if (fallingBlock.isOnGround()) fallingBlock.getLocation().getBlock().setType(Material.AIR);
         if (!fallingBlock.isDead()) fallingBlock.remove();
     }
-
-
+    
+    
     protected double random() {
         return -0.5D + ThreadLocalRandom.current().nextDouble();
     }
-
-    private void startEvent(Location location){
+    
+    private void startEvent(Location location) {
         Optional<SchematicImpl> worldEditSchematic = HyperDragons.getInstance().getSchematicManager().getSchematic(plugin.getConfiguration().schematic);
-        if(!worldEditSchematic.isPresent()) {
-            Bukkit.getConsoleSender().sendMessage(StringUtils.color("&c[UltimateCore-Dragon] Schematic "+plugin.getConfiguration().schematic+" wasn't found!"));
+        if (!worldEditSchematic.isPresent()) {
+            Bukkit.getConsoleSender().sendMessage(StringUtils.color("&c[UltimateCore-Dragon] Schematic " + plugin.getConfiguration().schematic + " wasn't found!"));
             startTimer();
             return;
         }
         worldEditSchematic.get().pasteSchematic(location).thenRun(this::startTimer);
     }
-
-
+    
+    
     public void startTimer() {
         spawnStand();
         new TimerPart(this, plugin.getConfiguration().spawnTime).startTimer().thenRun(this::makeBlockExplosion);
