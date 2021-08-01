cc4aebda675f906d3ea92e0c8b48db7ba0a9d15a
Eduard Wayland <eduproard@gmail.com>
2021-08-01 17:52:03 +0200

    Refactoring

--- UltimateAnvil/UltimateAnvilPlugin/src/mc/ultimatecore/anvil/commands/GiveBookItem.java
+++ UltimateAnvil/UltimateAnvilPlugin/src/mc/ultimatecore/anvil/commands/GiveBookItem.java
@@ -13,40 +13,40 @@ import java.util.Collections;
 import java.util.List;
 
 public class GiveBookItem extends Command {
-
+    
     public GiveBookItem() {
         super(Collections.singletonList("givebook"), "To Give Enchanted Book", "hyperanvil.givebook", true, "/HyperAnvil givebook <player> <enchantment> <level>");
     }
-
+    
     @Override
     public void execute(CommandSender sender, String[] args) {
         Player p = (Player) sender;
-        if(args.length == 4){
+        if (args.length == 4) {
             try {
                 String name = args[2];
                 int level = Integer.parseInt(args[3]);
                 HyperEnchant hyperEnchant = EnchantmentsPlugin.getInstance().getApi().getEnchantmentInstance(name);
-                if (hyperEnchant != null){
-                    if(level > hyperEnchant.getMaxLevel())
+                if (hyperEnchant != null) {
+                    if (level > hyperEnchant.getMaxLevel())
                         p.sendMessage(Utils.color(EnchantmentsPlugin.getInstance().getConfiguration().prefix + "&7Level can't be higher than enchantment max level!"));
                     else
                         p.getInventory().addItem(EnchantmentsPlugin.getInstance().getApi().getEnchantedBook(hyperEnchant, level));
-                }else{
-                    p.sendMessage(Utils.color(EnchantmentsPlugin.getInstance().getConfiguration().prefix+"&7Invalid Enchantment!"));
+                } else {
+                    p.sendMessage(Utils.color(EnchantmentsPlugin.getInstance().getConfiguration().prefix + "&7Invalid Enchantment!"));
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 p.sendMessage(StringUtils.color(HyperAnvil.getInstance().getMessages().getMessage("invalidArguments").replace("%prefix%", HyperAnvil.getInstance().getConfiguration().prefix)));
             }
         } else {
             p.sendMessage(StringUtils.color(HyperAnvil.getInstance().getMessages().getMessage("invalidArguments").replace("%prefix%", HyperAnvil.getInstance().getConfiguration().prefix)));
         }
     }
-
+    
     @Override
     public List<String> onTabComplete(CommandSender commandSender, org.bukkit.command.Command command, String label, String[] args) {
-        if(args.length == 3)
+        if (args.length == 3)
             return new ArrayList<>(EnchantmentsPlugin.getInstance().getHyperEnchantments().enchantments.keySet());
         return null;
     }
-
+    
 }
