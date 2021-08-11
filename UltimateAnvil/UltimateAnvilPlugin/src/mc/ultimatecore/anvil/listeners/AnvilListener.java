d7941f2b1ebcb2a58b594d6968393c47fb5fc6da
Eduard Wayland <eduproard@gmail.com>
2021-08-12 01:14:05 +0200

    Damaged Anvils will now open the anvil gui

--- UltimateAnvil/UltimateAnvilPlugin/src/mc/ultimatecore/anvil/listeners/AnvilListener.java
+++ UltimateAnvil/UltimateAnvilPlugin/src/mc/ultimatecore/anvil/listeners/AnvilListener.java
@@ -12,11 +12,12 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 public class AnvilListener implements Listener {
     @EventHandler
-    public void onOpenEnchantmentTable(PlayerInteractEvent e){
-        if(!HyperAnvil.getInstance().getConfiguration().setAsDefaultAnvil) return;
-        if(e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+    public void onOpenEnchantmentTable(PlayerInteractEvent e) {
+        if (!HyperAnvil.getInstance().getConfiguration().setAsDefaultAnvil) return;
+        if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         Block bl = e.getClickedBlock();
-        if(bl == null || !bl.getType().equals(XMaterial.ANVIL.parseMaterial())) return;
+        if (bl == null || (!bl.getType().equals(XMaterial.ANVIL.parseMaterial()) && !bl.getType().equals(XMaterial.DAMAGED_ANVIL.parseMaterial())))
+            return;
         e.setCancelled(true);
         Player p = e.getPlayer();
         User user = User.getUser(p);
