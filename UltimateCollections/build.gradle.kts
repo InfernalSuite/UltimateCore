72700ddbb562fe38d39485aac574b1a3b3cfcb83
Paul19988 <paul@paulcodes.co.uk>
2022-03-30 16:00:03 +0100

    Resolve incorrect dependency for NBT API

--- UltimateCollections/build.gradle.kts
+++ UltimateCollections/build.gradle.kts
@@ -2,11 +2,12 @@ version = "4.0.0"
 
 repositories {
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.codemc.org/repository/maven-public/")
 }
 
 dependencies {
     implementation("com.github.cryptomorin:XSeries:8.7.0")
-    implementation("de.tr7zw.changeme.nbtapi:item-nbt-api:2.9.2-SNAPSHOT")
+    implementation("de.tr7zw:item-nbt-api:2.9.2")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
     compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
