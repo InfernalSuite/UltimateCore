75a7ef7de844ad30cda9340c4999abd385869624
Paul19988 <paul@paulcodes.co.uk>
2022-03-30 20:21:53 +0100

    Update NBT API across all required.

--- UltimateSkills/build.gradle.kts
+++ UltimateSkills/build.gradle.kts
@@ -2,11 +2,12 @@ version = "4.0.0"
 
 repositories {
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://repo.codemc.org/repository/maven-public/")
 }
 
 dependencies {
     implementation("com.github.cryptomorin:XSeries:8.7.0")
-    implementation("de.tr7zw:item-nbt-api:2.9.0-SNAPSHOT")
+    implementation("de.tr7zw:item-nbt-api:2.9.2")
 
     compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
