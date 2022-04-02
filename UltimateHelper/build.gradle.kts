7d0b3b17bd53a7285a1e005f21642572bf05d2ae
Philip Kelley <philip@thoriumcube.org>
2022-04-02 10:00:28 +0100

    feat: Add guice

--- UltimateHelper/build.gradle.kts
+++ UltimateHelper/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("com.github.cryptomorin:XSeries:8.7.0")
     implementation("de.tr7zw:item-nbt-api:2.9.2")
     implementation("me.lucko:helper:5.6.9")
+    implementation("com.google.inject:guice:5.1.0")
     api("com.zaxxer:HikariCP:5.0.1")
 
     compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
