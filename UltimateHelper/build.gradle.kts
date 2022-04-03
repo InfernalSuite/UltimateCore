e148a9a809f21ac4f8e2d5dee99dcde6f14f2336
Philip Kelley <philip@thoriumcube.org>
2022-04-03 16:55:34 +0100

    Add dependency on API

--- UltimateHelper/build.gradle.kts
+++ UltimateHelper/build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation("de.tr7zw:item-nbt-api:2.9.2")
     implementation("me.lucko:helper:5.6.9")
     implementation("com.google.inject:guice:5.1.0")
+    implementation(project(":UC-API"))
+
     api("com.zaxxer:HikariCP:5.0.1")
 
     compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
