2578923c6fae5ff23fbd545597315cd137da797b
Philip Kelley <philip@thoriumcube.org>
2022-04-08 16:17:01 +0100

    Add isPlayerOnline check

--- UltimateHelper/src/main/java/mc/ultimatecore/helper/UltimatePlugin.java
+++ UltimateHelper/src/main/java/mc/ultimatecore/helper/UltimatePlugin.java
@@ -9,7 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class UltimatePlugin extends JavaPlugin {
 
@@ -30,6 +30,11 @@ public class UltimatePlugin extends JavaPlugin {
     public void onDisable() {
         super.onDisable();
         Bukkit.getOnlinePlayers().forEach(Player::closeInventory);
+    }
+
+    public boolean isPlayerOnline(UUID uuid) {
+        Player player = getServer().getPlayer(uuid);
+        return player != null && player.isOnline();
     }
     
     protected void registerListeners(Listener... listeners) {
