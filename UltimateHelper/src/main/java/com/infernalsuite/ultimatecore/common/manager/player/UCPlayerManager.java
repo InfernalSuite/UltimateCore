74b35d14e7c8ec93ba35413c03543fdeeb80c72e
Philip Kelley <philip@thoriumcube.org>
2022-04-08 15:54:48 +0100

    Add UCPlayerManager Interface

---
+++ UltimateHelper/src/main/java/com/infernalsuite/ultimatecore/common/manager/player/UCPlayerManager.java
@@ -0,0 +1,30 @@
+package com.infernalsuite.ultimatecore.common.manager.player;
+
+import com.infernalsuite.ultimatecore.common.manager.*;
+import com.infernalsuite.ultimatecore.common.player.*;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public interface UCPlayerManager<T extends UCPlayerImpl> extends Manager<UUID, UCPlayerImpl, T> {
+
+    T getOrMake(UUID id);
+
+    /**
+     * Gets a player object by name.
+     * @param name the name to search by
+     * @return a {@link UCPlayerImpl} object if the player is loaded, or {@code null}
+     */
+    T getByUsername(String name);
+
+    /**
+     * Reloads the data of all online players.
+     */
+    CompletableFuture<Void> loadAllPlayers();
+
+    /**
+     * Invalidates the cached data for loaded players.
+     */
+    void invalidateAllPlayerCaches();
+
+}
