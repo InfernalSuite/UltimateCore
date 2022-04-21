b73b85af367c86d07be53c3061e8e07ca2733b8e
Paul19988 <paul@paulcodes.co.uk>
2022-04-21 21:33:38 +0100

    Toolchain Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ allprojects {
 
     java {
         toolchain {
-            toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+            toolchain.languageVersion.set(JavaLanguageVersion.of(11))
         }
     }
 }
