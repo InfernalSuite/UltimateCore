fb6ee2b73e37f2c2ad29b7987086dd25983fcbfe
Paul19988 <paul@paulcodes.co.uk>
2022-04-21 21:40:53 +0100

    PACKAGE REMOVAL

--- UltimateHelper/v1_17_R1/src/main/java/mc/ultimatecore/helper/v1_17_R1/VersionHookImpl.java
+++ UltimateHelper/v1_17_R1/src/main/java/mc/ultimatecore/helper/v1_17_R1/VersionHookImpl.java
@@ -2,7 +2,6 @@ package mc.ultimatecore.helper.v1_17_R1;
 
 import mc.ultimatecore.helper.*;
 import mc.ultimatecore.helper.v1_17_R1.hooks.worldedit.WorldEditImpl;
-import mc.ultimatecore.helper.v1_18_R2.hooks.worldedit.*;
 
 public class VersionHookImpl implements VersionHook {
 
