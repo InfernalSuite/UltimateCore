6d8c2912be1c849f4c2b466880ec987455b7a5f3
Philip Kelley <philip@thoriumcube.org>
2022-04-21 17:38:19 +0100

    Fix: change to compileOnly

--- UltimateDragon/build.gradle.kts
+++ UltimateDragon/build.gradle.kts
@@ -17,5 +17,5 @@ dependencies {
     compileOnly("com.zaxxer:HikariCP:5.0.1")
     compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
 
-    implementation(project(":UltimateHelper"))
+    compileOnly(project(":UltimateHelper"))
 }
